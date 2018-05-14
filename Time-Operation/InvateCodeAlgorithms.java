361733484066f2eecd3623457b8ca7f7b2d1bc3a
xujifu <xujifu@gmail.com>
2018-05-14 14:18:58 +0800

    修改邀请码生成
    N/A
    
    修改部分代码，删除
    
    Signed-off-by: xujifu <xujifu@gmail.com>

--- Time-Operation/InvateCodeAlgorithms.java
+++ Time-Operation/InvateCodeAlgorithms.java
@@ -82,31 +82,6 @@ public class InvateCodeAlgorithms {
     //临时向量
     private char[] vectorT = new char[RC4_LENGTH];
 
-    private static final char[] BASE_CHAR = new char[]{
-        'D', 'X', 'G', 'W', 'F', 'K',
-        'T', 'R', '6', '9', '7', 'S',
-        'P', 'A', '4', 'V', 'Y', 'B',
-        'U', 'J', 'E', 'N', '8', '2',
-        'C', 'Z', 'H', '5', 'Q', 'L',
-        'M', '3'};
-
-    public String simplecode(long uid){
-        long tuid = uid;
-        StringBuffer sb = new StringBuffer();
-        while(tuid > 0){
-            sb.append(r[(int)(tuid % r.length)]);
-            tuid = tuid / r.length;
-        }
-        if(sb.length() < 6) {
-            Random r = new Random();
-            for (int i = 0; i < 6 - sb.length(); i++) {
-                int num = r.nextInt(this.r.length);
-                sb.append(this.r[num]);
-            }
-        }
-        return sb.toString();
-    }
-
     private void swap(int i, int j) {
         vectorS[i] ^= vectorS[j];
         vectorS[j] ^= vectorS[i];
@@ -277,81 +252,5 @@ public class InvateCodeAlgorithms {
 //            System.out.println(a.fcode(i) + "," + a.funcode(a.fcode(i)));
 //        }
 //        System.out.println(tree.size() - code.size());
-        System.out.println(a.toSerialCode(594552576));
-        System.out.println(a.simplecode(594552577));
-//        for(long i = 0; i < 25000000l; i ++){
-//            String str = a.toSerialCode(i);
-//            tree.add(str);
-//            code.add(str);
-//        }
-//        System.out.println(tree.size() - code.size());
     }
-
-    /** 自定义进制(0,1没有加入,容易与o,l混淆) */
-    private static final char[] r=new char[]{'q', 'w', 'e', '8', 'a', 's', '2', 'd', 'z', 'x', '9', 'c', '7', 'p', '5', 'i', 'k', '3', 'm', 'j', 'u', 'f', 'r', '4', 'v', 'y', 'l', 't', 'n', '6', 'b', 'g', 'h'};
-
-    /** (不能与自定义进制有重复) */
-    private static final char b='o';
-
-    /** 进制长度 */
-    private static final int binLen=r.length;
-
-    /** 序列最小长度 */
-    private static final int s=6;
-
-    /**
-     * 根据ID生成六位随机码
-     * @param id ID
-     * @return 随机码
-     */
-    public static String toSerialCode(long id) {
-        char[] buf=new char[32];
-        int charPos=32;
-
-        while((id / binLen) > 0) {
-            int ind=(int)(id % binLen);
-            // System.out.println(num + "-->" + ind);
-            buf[--charPos]=r[ind];
-            id /= binLen;
-        }
-        buf[--charPos]=r[(int)(id % binLen)];
-        // System.out.println(num + "-->" + num % binLen);
-        String str=new String(buf, charPos, (32 - charPos));
-        // 不够长度的自动随机补全
-        if(str.length() < s) {
-            StringBuilder sb=new StringBuilder();
-            sb.append(b);
-            Random rnd=new Random();
-            for(int i=1; i < s - str.length(); i++) {
-                sb.append(r[rnd.nextInt(binLen)]);
-            }
-            str+=sb.toString();
-        }
-        return str;
-    }
-
-    public static long codeToId(String code) {
-        char chs[]=code.toCharArray();
-        long res=0L;
-        for(int i=0; i < chs.length; i++) {
-            int ind=0;
-            for(int j=0; j < binLen; j++) {
-                if(chs[i] == r[j]) {
-                    ind=j;
-                    break;
-                }
-            }
-            if(chs[i] == b) {
-                break;
-            }
-            if(i > 0) {
-                res=res * binLen + ind;
-            } else {
-                res=ind;
-            }
-            // System.out.println(ind + "-->" + res);
-        }
-        return res;
-    }
-
 }
